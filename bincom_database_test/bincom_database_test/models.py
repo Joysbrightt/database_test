e757fa31b4f9596c3b3d0dfbdc280cef44588331
Joysbrightt <oladoyinoluwatomisin11@gmail.com>
2023-07-07 16:33:03 +0100

    "refactor create_polling_unit.html file and updated the database and models.py"

--- bincom_database_test/bincom_database_test/models.py
+++ bincom_database_test/bincom_database_test/models.py
@@ -7,11 +7,17 @@ class PollingUnitResult(models.Model):
     ward_id = models.IntegerField()
     lga_id = models.IntegerField()
     state_id = models.IntegerField()
+    pdp_votes = models.IntegerField()
+    dpp_votes = models.IntegerField()
+    acn_votes = models.IntegerField()
+    cdc_votes = models.IntegerField()
+    jp_votes = models.IntegerField()
 
     def __str__(self):
         return str(self.uniqueid)
 
     # Add other fields as required
+
 
 # Define other models (Ward, LGA, announced_pu_results, announced_lga_results) in a similar manner
 class PartyResult:
